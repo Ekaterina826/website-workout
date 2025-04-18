7270c7c943eac97635e4ddb7cbe18070671cd4bc
Ekaterina Timofeeva <ketrinkot14@gmail.com>
2025-04-18 14:21:07 +0300

    Добавила главную страницу

--- scripts/main.js
+++ scripts/main.js
@@ -14,21 +14,3 @@ document.querySelector(".modern-button").addEventListener("click", function () {
     this.style.transform = "scale(1)";
   }, 200);
 });
-document.addEventListener("DOMContentLoaded", function () {
-  // Создаем частицы фона
-  const container = document.getElementById("particles");
-  const colors = ["#7ba0d6", "#241d9a", "#a277e3", "#ff69b4"];
-
-  for (let i = 0; i < 30; i++) {
-    const particle = document.createElement("div");
-    particle.className = "particle";
-    particle.style.left = Math.random() * 100 + "%";
-    particle.style.top = Math.random() * 100 + "%";
-    particle.style.background = `radial-gradient(circle, ${colors[i % 4]}, ${
-      colors[(i + 1) % 4]
-    })`;
-    particle.style.animationDelay = Math.random() * 5 + "s";
-    particle.style.animationDuration = 5 + Math.random() * 10 + "s";
-    container.appendChild(particle);
-  }
-});
